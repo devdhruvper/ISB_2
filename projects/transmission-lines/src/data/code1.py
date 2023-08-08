5df32914f11ed65bedf4e6b3d0fab9ad9e13c08d
devdhruvper <talanp232@gmail.com>
2023-08-08 21:34:18 +0530

    Updated code has been pushed and data file is also included

--- projects/transmission-lines/src/data/code1.py
+++ projects/transmission-lines/src/data/code1.py
@@ -1,7 +1,22 @@
 import requests
 import pandas as pd
-path='C:\Users\dhruv\OneDrive\Desktop\ML_projects\ML_project_5\ISB\BIPP\New folder\bipp-datasets\projects\transmission-lines\data\raw'
+from pathlib import Path
+import pandas as pd
+
+def Date_convert(month :str):
+ date_string = "jun-23"
+ date = pd.to_datetime(date_string, format="%b-%y")
+ formatted_date = date.strftime("%d-%m-%Y")
+ return formatted_date
+
+
+path=Path.cwd()
+raw_path = Path.joinpath(path,"data","raw",'transmission_lines.csv')
 url="https://cea.nic.in/api/transmission_lines.php"
 Response=requests.get(url)
+
+
 DF=pd.read_json(Response.text)
-DF.to_csv('Transmission_lines.csv',index=False)
+DF['month'] = DF['month'].apply(Date_convert)
+DF.rename(columns = {'month':'date'}, inplace = True)
+DF.to_csv(raw_path,index=False)
