7f5d7bf40b74fc95b260538a9750e1b9a727e6cd
devdhruvper <talanp232@gmail.com>
2023-08-15 21:04:45 +0530

    Added the commits to my Task1.py file

--- projects/transmission-lines/src/data/Task1.py
+++ projects/transmission-lines/src/data/Task1.py
@@ -3,20 +3,21 @@ import pandas as pd
 from pathlib import Path
 import pandas as pd
 
+#Defining a function which converts the months column into suitable date format 
 def Date_convert(month :str):
  date_string = "jun-23"
  date = pd.to_datetime(date_string, format="%b-%y")
  formatted_date = date.strftime("%d-%m-%Y")
  return formatted_date
 
-
+#use the path library from pathlib module to set the path of our csv
 path=Path.cwd()
 raw_path = Path.joinpath(path,"data","raw",'transmission_lines.csv')
-url="https://cea.nic.in/api/transmission_lines.php"
-Response=requests.get(url)
+url="https://cea.nic.in/api/transmission_lines.php"   
+Response=requests.get(url) #Fetching the data from the given url
 
 
 DF=pd.read_json(Response.text)
-DF['month'] = DF['month'].apply(Date_convert)
-DF.rename(columns = {'month':'date'}, inplace = True)
-DF.to_csv(raw_path,index=False)
+DF['month'] = DF['month'].apply(Date_convert) #applying the function to our column 
+DF.rename(columns = {'month':'date'}, inplace = True) #renaming
+DF.to_csv(raw_path,index=False) #converting to csv
